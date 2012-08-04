90942f9fbefcd79700c31d913e4b2522c4eef666
Ryan Riley <ryan.riley@panesofglass.org>
2012-08-04 14:24:57 -0500

    Include LAgent source and samples.

---
+++ MailboxProcessors.fsx
@@ -0,0 +1,17 @@
+﻿#load "AgentSystem.fs"
+open AgentSystem.LAgent
+
+let counter0 =
+    MailboxProcessor.Start(fun inbox ->
+        let rec loop n =
+            async { 
+                    let! msg = inbox.Receive()
+                    return! loop(n+msg) }
+        loop 0)
+
+counter0.Post(3)
+
+let counter1 = MailboxProcessor.SpawnAgent( (fun msg n -> msg + n), 0)
+
+counter1.Post(3)
+
---
+++ MultipleWorkers.fsx
@@ -0,0 +1,19 @@
+﻿#load "AgentSystem.fs"
+open AgentSystem.LAgent
+open System
+open System.Threading
+
+let tprintfn s = printfn "Executing %s on thread %i" s Thread.CurrentThread.ManagedThreadId
+let paralleltprintfn s =
+    printfn "Executing %s on thread %i" s Thread.CurrentThread.ManagedThreadId
+    Thread.Sleep(300)
+
+let echo = MailboxProcessor<_>.SpawnWorker(tprintfn)
+let echos = MailboxProcessor.SpawnParallelWorker(paralleltprintfn, 10)
+
+let messages = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"l";"m";"n";"o";"p";"q";"r";"s";"t"]
+printfn "...Just one guy doing the work"
+messages |> Seq.iter (fun msg -> echo.Post(msg))
+Thread.Sleep 1000
+printfn "...With a little help from his friends"
+messages |> Seq.iter (fun msg -> echos.Post(msg))
